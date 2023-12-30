2df54e6ceb1311ec8c044df98e366555dc338d3e
davemackintosh <me@davemackintosh.co.uk>
2023-12-30 17:38:14 +0000

    Rethink

--- src/ast/parsers/funcs.rs
+++ src/ast/parsers/funcs.rs
@@ -21,54 +21,21 @@ pub fn parse_fn(parser: &mut Parser) -> Result<Fn, SyntaxError> {
         },
     };
 
-    while parser.consume().is_some() {
-        if let Some(token) = &parser.current_token {
-            match &token.token_type {
-                TokenType::Symbol(Symbol::OpenParen) => continue,
-                TokenType::Ident(ident) => match parser.peek() {
-                    Ok(next_token) => match next_token.token_type {
-                        TokenType::Symbol(Symbol::Colon) => {
-                            // Peek doesn't consume the token so we grab the next 2 to check the
-                            // second is an ident.
-                            let tokens = parser.consume_n(2);
+    let mut param = Parameter {
+        name: Identifier {
+            immutable: true,
+            access_modifier: Pub,
+            name: "".into(),
+        },
+        r#type: Type::TypeName(Identifier {
+            immutable: true,
+            access_modifier: Pub,
+            name: "".into(),
+        }),
+    };
 
-                            return match tokens {
-                                Ok(tokens) => match tokens.get(1) {
-                                    Some(type_hint) => match type_hint.token_type {
-                                        TokenType::Keyword(crate::ast::lexer::Keyword::Fn) => {
-                                            parse_interface_declaration(parser)
-                                        }
-                                        _ => {
-                                            return Err(SyntaxError::UnexpectedTokenButGot(
-                                                TokenType::Symbol(Symbol::Comma),
-                                                parser.current_token.clone().unwrap(),
-                                            ));
-                                        }
-                                    },
-                                    None => return Err(SyntaxError::MissingToken("any")),
-                                },
-                                Err(_) => return Err(SyntaxError::MissingToken("ident")),
-                            };
-                        }
-                        _ => {
-                            return Err(SyntaxError::UnexpectedTokenButGot(
-                                TokenType::Symbol(Symbol::Comma),
-                                parser.current_token.clone().unwrap(),
-                            ));
-                        }
-                    },
-                    Err(_) => {
-                        return Err(SyntaxError::MissingToken("any"));
-                    }
-                },
-                _ => {
-                    return Err(SyntaxError::UnexpectedToken(
-                        parser.current_token.clone().unwrap(),
-                    ));
-                }
-            }
-        }
-    }
+    // the next 3 tokens could/should be an ident, opening parenthesis and parameter.
+    let next_tokens = parser.consume_n(3);
 
     Ok(fn_declaration)
 }
